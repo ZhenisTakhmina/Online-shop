9653c9038c47db0bdcc716d53ed506a443ec7ef0
Zhenis Madiyar <jenis.madiyar@gmail.com>
2024-06-05 01:35:03 +0500

    Initial Commit

---
+++ onlineShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onlineShop
+//
+//  Created by Madiyar Zhenis on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
